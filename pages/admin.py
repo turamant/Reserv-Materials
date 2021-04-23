82bb3fb8fbf149343938f14b5077fb1c673e7dde
askvart <tur1amant@gmail.com>
2021-04-23 16:53:27 +0300

    add get_image in admin.py

--- pages/admin.py
+++ pages/admin.py
@@ -1,4 +1,5 @@
 from django.contrib import admin
+from django.utils.safestring import mark_safe
 
 # Register your models here.
 from pages.models import Note
@@ -7,6 +8,12 @@ from pages.models import Note
 @admin.register(Note)
 class NoteAdmin(admin.ModelAdmin):
     list_display = ('name','family','slug',
-                    'image','price','quantity',
-                    'computing_price','nalog_nds')
+                    "get_image",'price','quantity',
+                    'computing_price','nalog_nds','computing_price')
+    readonly_fields = ("get_image",)
     prepopulated_fields = {"slug":('name',),}
+
+    def get_image(self, obj):
+        return mark_safe(f'<img src={obj.image.url} width="50" height="60"')
+
+    get_image.short_description = "Изображение"
